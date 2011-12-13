0e25fb835f6ca76b6065ad94eada3744062a604e
Michael Hoy <michael.john.hoy@gmail.com>
2011-12-13 16:58:48 -0500

    ruby should check whether values are numbers and convert before converting to json

--- src/convert_csv_to_json.rb
+++ src/convert_csv_to_json.rb
@@ -6,20 +6,38 @@
 require 'csv'
 require 'fileutils'
 require 'json'
+require 'pp'
 
 files = Dir['data/*.csv']
 output_dir = 'json'
 FileUtils.mkdir_p output_dir
 
+# Takes an array of hashes to be exported as JSON. We need
+# to convert all number-like strings to numbers.
+def clean(arr)
+  arr.map do |hsh|
+    obj = {}
+    hsh.each do |key, value|
+      if value and value.match /^-?\d+(\.\d+)?$/
+        value = value.to_f
+      end
+      obj[key] = value
+    end
+    obj
+  end
+end
+
 files.each do |path|
 
-  arr = []
+  raw_arr = []
 
   CSV.open(path, { :headers => true }) do |csv|
     csv.each do |row|
-      arr.push row.to_hash
+      raw_arr.push row.to_hash
     end
   end
+
+  arr = clean(raw_arr)
 
   # Output the JSON data.
   name = File.basename(path).gsub(/#{File.extname(path)}$/, '')
